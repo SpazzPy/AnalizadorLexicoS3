b98ef82ede426158f03050549b035a50f6e22066
Spazz <91347861+SpazzPy@users.noreply.github.com>
2023-10-08 21:45:28 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+# Importa la biblioteca math
+import math
+
+# Define una clase
+class MiClase:
+    # Define una funcion dentro de la clase
+    def mi_funcion(self, x):
+        # Utiliza una funcion de la biblioteca math
+        return math.sqrt(x)
+
+# Crea una instancia de la clase
+mi_objeto = MiClase()
+
+# Utiliza la funcion de la instancia de la clase
+print(mi_objeto.mi_funcion(16))
